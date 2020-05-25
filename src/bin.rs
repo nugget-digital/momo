d78bdcffe810c160dc2a203b30d8aa37cfb943c8
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-05-25 12:35:52 +0200

    fix: http header name casing

--- src/bin.rs
+++ src/bin.rs
@@ -1,19 +1,24 @@
-use anyhow::{anyhow, Result};
+use anyhow::{bail, Result};
 use http::StatusCode;
 use reqwest::blocking;
-use serde::{Deserialize, Serialize};
-use serde_json::{json, to_string};
+use serde::Deserialize;
+use serde_json::json;
 use structopt::StructOpt;
 use uuid::Uuid;
 
-#[derive(Debug, Serialize)]
-struct SandboxCredentials {
+mod common;
+
+use common::FALLBACK_CALLBACK_HOST;
+
+#[derive(Debug)]
+struct SandboxUser {
     username: String,
     password: String,
+    callback_host: String,
 }
 
 #[derive(Debug, StructOpt)]
-struct Konfu {
+struct CliConfig {
     #[structopt(long = "callback-host")]
     callback_host: Option<String>,
     #[structopt(long = "subscription-key")]
@@ -26,29 +31,41 @@ struct SandboxApiKey {
     apiKey: String,
 }
 
-fn create_sandbox_credentials(
-    callback_host: &str,
-    subscription_key: &str,
-) -> Result<SandboxCredentials> {
-    let client = blocking::Client::new();
+fn create_sandbox_credentials(cli_config: &CliConfig) -> Result<SandboxUser> {
+    let http_client = blocking::Client::builder()
+        .http1_title_case_headers()
+        .build()?;
 
-    let body = json!({ "providerCallbackHost": callback_host }).to_string();
+    // callback_host must be the host for all callback urls used with
+    // the to be created sandbox api user credentials
+    let callback_host = if let Some(host) = &cli_config.callback_host {
+        host
+    } else {
+        // there is a mock PUT endpoint @
+        // https://www.mocky.io/v2/5ec0fa1c2f000079004c86fb
+        println!(
+            "[mini-mtn-momo] using fallback callback host {}",
+            FALLBACK_CALLBACK_HOST
+        );
 
-    let user_id = Uuid::new_v4();
-    let user_id_string = user_id.to_string();
+        FALLBACK_CALLBACK_HOST
+    };
 
-    let response = client
+    let user_id_string = Uuid::new_v4().to_string();
+
+    let response = http_client
         .post("https://sandbox.momodeveloper.mtn.com/v1_0/apiuser")
+        .header("Ocp-Apim-Subscription-Key", &cli_config.subscription_key)
         .header("X-Reference-Id", &user_id_string)
-        .header("Ocp-Apim-Subscription-Key", subscription_key)
-        .json(&body)
+        .header("Content-Type", "application/json")
+        .body(json!({ "providerCallbackHost": callback_host }).to_string())
         .send()?;
 
     if response.status() != StatusCode::CREATED {
-        return Err(anyhow!(
+        bail!(
             "creating a sandbox user failed - http status {:?}",
             response.status()
-        ));
+        );
     }
 
     let url = format!(
@@ -56,43 +73,35 @@ fn create_sandbox_credentials(
         &user_id_string,
     );
 
-    let response = client
+    let response = http_client
         .post(&url)
+        .header("Ocp-Apim-Subscription-Key", &cli_config.subscription_key)
         .header("X-Reference-Id", &user_id_string)
-        .header("Ocp-Apim-Subscription-Key", subscription_key)
+        .header("Content-Length", "0")
         .send()?;
 
     if response.status() != StatusCode::CREATED {
-        return Err(anyhow!(
+        bail!(
             "creating a sandbox api key failed - http status {:?}",
             response.status()
-        ));
+        );
     }
 
     let api_key = response.json::<SandboxApiKey>()?.apiKey;
 
-    Ok(SandboxCredentials {
+    Ok(SandboxUser {
         username: user_id_string,
         password: api_key,
+        callback_host: callback_host.to_string(),
     })
 }
 
 pub fn main() -> Result<()> {
-    let konfu = Konfu::from_args();
+    let cli_config = CliConfig::from_args();
 
-    let callback_host = if let Some(host) = &konfu.callback_host {
-        host
-    } else {
-        // callback_host must be the host for all callback urls used
-        // mock PUT https://www.mocky.io/v2/5ec0fa1c2f000079004c86fb
-        // NOTE: fallback hardcoded bc required when creating a sandbox user
-        "mocky.io"
-    };
+    let sandbox_credentials = create_sandbox_credentials(&cli_config)?;
 
-    let sandbox_credentials =
-        create_sandbox_credentials(callback_host, &konfu.subscription_key)?;
-
-    println!("{}", to_string(&sandbox_credentials)?);
+    println!("{:?}", &sandbox_credentials);
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-use anyhow::{anyhow, Result};
+use anyhow::{bail, Error, Result};
 use http::StatusCode;
 use reqwest::blocking;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use uuid::Uuid;
+
+use std::str::FromStr;
+
+mod common;
+
+use common::*;
 
 #[repr(C)]
 #[derive(Debug, Serialize, Deserialize)]
@@ -13,9 +19,25 @@ pub enum PaymentStatus {
     Pending,
 }
 
+impl FromStr for PaymentStatus {
+    type Err = Error;
+
+    fn from_str(status: &str) -> Result<PaymentStatus> {
+        let payment_status = match status {
+            "SUCCESSFUL" => PaymentStatus::Resolved,
+            "FAILED" => PaymentStatus::Rejected,
+            "PENDING" => PaymentStatus::Pending,
+            _ => bail!("unknown payment status {:?}", status),
+        };
+
+        Ok(payment_status)
+    }
+}
+
 #[allow(non_snake_case)]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Balance {
+    #[serde(rename(deserialize = "available_balance"))]
     availableBalance: String,
     currency: String,
 }
@@ -26,17 +48,22 @@ pub struct Config {
     pub password: String,
     pub subscription_key: String,
     pub base_url: Option<String>,
-    pub callback_url: Option<String>,
+    pub callback_host: Option<String>,
+    pub device_id: Option<String>,
 }
 
 #[derive(Debug)]
 pub struct Client {
-    config: Config,
     http_client: blocking::Client,
-    collections_access_token: String,
     target_environment: String,
+    username: String,
+    password: String,
+    subscription_key: String,
+    collections_access_token: String,
     base_url: String,
+    callback_host: String,
     reauthorize: bool,
+    metadata: String,
 }
 
 #[derive(Deserialize)]
@@ -66,23 +93,26 @@ struct Payment {
 
 pub trait IClient {
     fn new(config: &Config) -> Result<Client>;
+    fn authorize_collections(&mut self) -> Result<&Client>;
     fn request_to_pay(
         &mut self,
         amount: u64,
         currency: &str,
         mobile_number: &str,
+        callback_url: Option<&str>,
     ) -> Result<Uuid>;
     fn request_to_pay_status(
         &mut self,
         reference_id: &Uuid,
     ) -> Result<PaymentStatus>;
     fn get_balance(&mut self) -> Result<Balance>;
-    fn authorize_collections(&mut self) -> Result<&Self>;
 }
 
 impl IClient for Client {
     fn new(config: &Config) -> Result<Client> {
-        let http_client = blocking::Client::new();
+        let http_client = blocking::Client::builder()
+            .http1_title_case_headers()
+            .build()?;
 
         let base_url;
         let target_environment;
@@ -94,29 +124,52 @@ impl IClient for Client {
                 base_url = format!("{}/", url);
             };
 
-            if url.starts_with("https://momodeveloper.mtn.com") {
-                target_environment = "production";
+            if url.starts_with(PRODUCTION_BASE_URL) {
+                target_environment = PRODUCTION;
             } else {
-                target_environment = "sandbox";
-            }
+                target_environment = SANDBOX;
+            };
         } else {
-            base_url = "https://sandbox.momodeveloper.mtn.com/".to_string();
-            target_environment = "sandbox";
+            println!(
+                "[mini-mtn-momo] using fallback sandbox environment \
+                located @ {}",
+                SANDBOX_BASE_URL
+            );
+
+            base_url = SANDBOX_BASE_URL.to_string();
+            target_environment = SANDBOX;
+        };
+
+        let callback_host = if let Some(domain) = &config.callback_host {
+            domain
+        } else {
+            println!(
+                "[mini-mtn-momo] using fallback callback host \"{}\"",
+                FALLBACK_CALLBACK_HOST
+            );
+
+            FALLBACK_CALLBACK_HOST
+        };
+
+        let device_id = if let Some(id) = &config.device_id {
+            id
+        } else {
+            println!("[mini-mtn-momo] using fallback device id \"unknown\"");
+
+            "unknown"
         };
 
         let mut client = Client {
-            config: Config {
-                username: config.username.clone(),
-                password: config.password.clone(),
-                subscription_key: config.subscription_key.clone(),
-                base_url: config.base_url.clone(),
-                callback_url: config.callback_url.clone(),
-            },
             http_client,
-            collections_access_token: "".to_string(),
             target_environment: target_environment.to_string(),
+            username: config.username.clone(),
+            password: config.password.clone(),
+            subscription_key: config.subscription_key.clone(),
+            collections_access_token: "".to_string(),
             base_url,
+            callback_host: callback_host.to_string(),
             reauthorize: true,
+            metadata: json!({ "device_id": device_id }).to_string(),
         };
 
         client.authorize_collections()?;
@@ -124,7 +177,7 @@ impl IClient for Client {
         Ok(client)
     }
 
-    fn authorize_collections(&mut self) -> Result<&Self> {
+    fn authorize_collections(&mut self) -> Result<&Client> {
         self.reauthorize = false;
 
         let url = format!("{}collection/token/", &self.base_url);
@@ -132,15 +185,15 @@ impl IClient for Client {
         let response = self
             .http_client
             .post(&url)
-            .basic_auth(&self.config.username, Some(&self.config.password))
-            .header("Ocp-Apim-Subscription-Key", &self.config.subscription_key)
+            .basic_auth(&self.username, Some(&self.password))
+            .header("Ocp-Apim-Subscription-Key", &self.subscription_key)
             .send()?;
 
         if response.status() != StatusCode::OK {
-            return Err(anyhow!(
+            bail!(
                 "authorizing collections failed - http status {:?}",
                 response.status()
-            ));
+            );
         } else {
             self.collections_access_token =
                 response.json::<Authorization>()?.access_token;
@@ -156,26 +209,37 @@ impl IClient for Client {
         amount: u64,
         currency: &str,
         mobile_number: &str,
+        callback_url: Option<&str>,
     ) -> Result<Uuid> {
         let url = format!("{}collection/v1_0/requesttopay/", &self.base_url);
 
         let reference_id = Uuid::new_v4();
         let reference_id_string = reference_id.to_string();
 
-        let callback_url = if let Some(url) = &self.config.callback_url {
+        let cb_url = if let Some(url) = callback_url {
             url
+        } else if self.callback_host.ends_with("mocky.io") {
+            println!(
+                "[mini-mtn-momo] using fallback callback url \"{}\"",
+                FALLBACK_CALLBACK_URL
+            );
+
+            FALLBACK_CALLBACK_URL
         } else {
-            ""
+            bail!(
+                "when having specified a custom callback host a callback url \
+                 with the same host is required for every request to pay"
+            );
         };
 
         let response = self
             .http_client
             .post(&url)
             .bearer_auth(&self.collections_access_token)
-            .header("X-Callback-Url", callback_url)
+            .header("X-Callback-Url", cb_url)
             .header("X-Reference-Id", &reference_id_string)
             .header("X-Target-Environment", &self.target_environment)
-            .header("Ocp-Apim-Subscription-Key", &self.config.subscription_key)
+            .header("Ocp-Apim-Subscription-Key", &self.subscription_key)
             .json(&json!({
                 "amount": amount,
                 "currency": currency,
@@ -186,7 +250,7 @@ impl IClient for Client {
                   "partyId": mobile_number,
                 },
                 "payerMessage": "it's time to pay :)",
-                "payeeNote": "string"
+                "payeeNote": &self.metadata,
             }))
             .send()?;
 
@@ -195,15 +259,17 @@ impl IClient for Client {
         if status == StatusCode::ACCEPTED {
             Ok(reference_id)
         } else if status == StatusCode::UNAUTHORIZED && self.reauthorize {
+            println!("currently unauthorized, attempting reauthorization...");
+
             self.authorize_collections()?;
 
-            self.request_to_pay(amount, currency, mobile_number)
+            self.request_to_pay(amount, currency, mobile_number, callback_url)
         } else {
-            Err(anyhow!(
+            bail!(
                 "payment request failed - http status {:?} - reference id {}",
                 response.status(),
                 reference_id_string,
-            ))
+            );
         }
     }
 
@@ -222,7 +288,7 @@ impl IClient for Client {
             .get(&url)
             .bearer_auth(&self.collections_access_token)
             .header("X-Target-Environment", &self.target_environment)
-            .header("Ocp-Apim-Subscription-Key", &self.config.subscription_key)
+            .header("Ocp-Apim-Subscription-Key", &self.subscription_key)
             .send()?;
 
         let status = response.status();
@@ -230,26 +296,22 @@ impl IClient for Client {
         if status == StatusCode::OK {
             let payment_status_string = response.json::<Payment>()?.status;
 
-            let payment_status = match &payment_status_string[..] {
-                "SUCCESSFUL" => PaymentStatus::Resolved,
-                "FAILED" => PaymentStatus::Rejected,
-                "PENDING" => PaymentStatus::Pending,
-                _ => {
-                    return Err(anyhow!("unknown payment status {:?}", status))
-                }
-            };
+            let payment_status =
+                PaymentStatus::from_str(&payment_status_string[..])?;
 
             Ok(payment_status)
         } else if status == StatusCode::UNAUTHORIZED && self.reauthorize {
+            println!("currently unauthorized, attempting reauthorization...");
+
             self.authorize_collections()?;
 
             self.request_to_pay_status(&reference_id)
         } else {
-            Err(anyhow!(
+            bail!(
                     "requesting payment status failed - http status {:?} - reference id {}",
                     response.status(),
                     reference_id.to_string(),
-                ))
+                );
         }
     }
 
@@ -261,7 +323,7 @@ impl IClient for Client {
             .get(&url)
             .bearer_auth(&self.collections_access_token)
             .header("X-Target-Environment", &self.target_environment)
-            .header("Ocp-Apim-Subscription-Key", &self.config.subscription_key)
+            .header("Ocp-Apim-Subscription-Key", &self.subscription_key)
             .send()?;
 
         let status = response.status();
@@ -271,14 +333,16 @@ impl IClient for Client {
 
             Ok(balance)
         } else if status == StatusCode::UNAUTHORIZED && self.reauthorize {
+            println!("currently unauthorized, attempting reauthorization...");
+
             self.authorize_collections()?;
 
             self.get_balance()
         } else {
-            Err(anyhow!(
+            bail!(
                 "getting wallet balance failed - http status {:?}",
                 response.status(),
-            ))
+            );
         }
     }
 }
